399aa3687a7089f3a7d5c7b1ee07f270bfd3cb98
Rafael Rocha <rocha.rafaelsilva@gmail.com>
2018-07-24 04:26:04 -0300

    Update function names in main.d.ts

--- main.d.ts
+++ main.d.ts
@@ -1,8 +1,8 @@
 // Type definitions for utf8-buffer 0.0.1
 // Project: https://github.com/rochars/utf8-buffer
-// Definitions by: Rafael S. Rocha <https://github.com/rochars>
+// Definitions by: Rafael da Silva Rocha <https://github.com/rochars>
 // Definitions: https://github.com/rochars/utf8-buffer
 
-export function unpackUTF8(buffer: Uint8Array|ArrayLike<number>, index?: number, len?: number): string;
+export function unpack(buffer: Uint8Array|ArrayLike<number>, index?: number, len?: number): string;
 
-export function packUTF8(str: string): ArrayLike<number>;
+export function pack(str: string): Uint8Array;

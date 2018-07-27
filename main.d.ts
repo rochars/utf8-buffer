46cf3dfa630aa5374199e8149f41d67ad33ec80a
Rafael Rocha <rocha.rafaelsilva@gmail.com>
2018-07-26 21:27:56 -0300

    Remove TypeScript declaration file

--- main.d.ts
+++
@@ -1,8 +0,0 @@
-// Type definitions for utf8-buffer 0.0.1
-// Project: https://github.com/rochars/utf8-buffer
-// Definitions by: Rafael da Silva Rocha <https://github.com/rochars>
-// Definitions: https://github.com/rochars/utf8-buffer
-
-export function unpack(buffer: Uint8Array|ArrayLike<number>, index?: number, len?: number): string;
-
-export function pack(str: string): Uint8Array;
